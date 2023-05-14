23271c9956608f2bb826c20a119e0b1d23a2a0bf
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2023-05-14 09:47:22 +0530

    Time: 46 ms (97.84%), Space: 41.7 MB (74.67%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const ret = [];
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)){
+            ret.push(arr[i]);
+        }
+
+    }
+    return ret;
+};
