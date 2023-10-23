79b1d249df7b818d7595ac2b30796e9a8965165b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2023-10-23 07:36:38 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return false;
+        
         float ans = log_a_to_base_b((float)n, 4.0);
-        // cout << ans;
         return (float)(ans - int(ans)) == 0.0;
     }
     
