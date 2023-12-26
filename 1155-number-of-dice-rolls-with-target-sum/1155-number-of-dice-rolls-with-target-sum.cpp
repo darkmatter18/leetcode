2971eca5c232e991f698f7e9b7bfd3b4360c2153
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2023-12-26 20:41:54 +0530

    Time: 13 ms (90.33%), Space: 10.5 MB (23.80%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    long helper(int n, int k, int target, map<pair<int, int>, long> &m){
+    long helper(int n, int k, int target, long **data){
         long MOD = 1e9+7;
         if(n == 0 && target == 0) return 1;
         if(n <= 0 || target <= 0) return 0;
         
-        pair<int, int> p(n, target);
-        
-        if(m.find(p) != m.end()) return m[p];
+        if(data[n][target] != -1) return data[n][target];
         
         long total = 0;
         for(int i = 1; i <= k; i++){
-            total += helper(n-1, k, target-i, m);
+            total += helper(n-1, k, target-i, data);
         }
         
         long ans = total % MOD;
-        m[p] = ans;
-        // cout << n << " " << k << " " << target << " " << ans << endl;
+        data[n][target] = ans;
         return total % MOD;
         
     }
 public:
     int numRollsToTarget(int n, int k, int target) {
-        map<pair<int, int>, long> m;
-        return (int) helper(n, k, target, m);
+        // map<pair<int, int>, long> m;
+        
+        long **data = new long*[n+1];
+        for(int i = 0; i < n+1; i++){
+            data[i] = new long[target+1];
+            for(int j = 0;j < target+1; j++){
+                data[i][j] = -1;
+            }
+        }
+        
+        return (int) helper(n, k, target, data);
     }
 };
