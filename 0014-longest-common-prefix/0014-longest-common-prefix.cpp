fd0269f3660f5f51b9b3424c5a7d998a081e8a5a
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2024-01-17 17:51:12 +0530

    Time: 6 ms (35.51%), Space: 9.5 MB (60.15%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
-        int max_len = 202;
+        int max_len = 202, min_len = 0;
         for(string s: strs){
-            if(max_len > s.size())
-                max_len = s.size(); 
+            max_len = min(max_len, (int)s.size());
         }
-        cout << "Max Len:  " << max_len << endl; 
         
-        int min_len = 0;
-        
-        if(max_len == 0){
-            return "";
-        }
+        if(max_len == 0) return "";
         
         for(int i = 1; i < strs.size(); i++){
             for(int m = 0; m < max_len; m++){
-                // cout << "M Val:  "<< m << strs[i-1][m] << " " << strs[i][m] << endl;
                 if(strs[i-1][m] != strs[i][m]){
-                    // cout << "T";
                     if(m == 0){
-                        cout << "ml u";
                         min_len = -1;
                     }
                     break;
                 }
                 min_len = m;
             }
-            cout << "ML: " << min_len;
             max_len = min_len+1;
             if(min_len == -1)
                 break;
