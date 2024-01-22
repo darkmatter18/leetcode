d146f39b08b8e32e58919c12771a036c58187355
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2024-01-22 10:48:25 +0530

    Time: 67 ms (17.81%), Space: 35.4 MB (8.31%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        set<int> s;
+        int size = nums.size();
+        int res = -1, total = 0;
+        
+        for(int n : nums){
+            if(s.find(n) == s.end()){
+                s.insert(n);
+                total += n;
+            }
+            else {
+                // n
+                res = n;
+            }
+        }
+        return {res, (size * (size+1) / 2) - total };
+        
+    }
+};
