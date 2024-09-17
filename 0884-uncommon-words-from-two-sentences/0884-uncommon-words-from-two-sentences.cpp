2b2354e7a5a5bf344cfe96db46f33183e3c8fe93
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2024-09-17 20:00:59 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (28.22%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -4,42 +4,36 @@ public:
         unordered_map<string, int> data;
         vector<string> res;
         
+        // String 1
         size_t pos = s1.find(' ');
         size_t initialPos = 0;
         string temp;
 
-        // Decompose statement
         while( pos != string::npos ) {
-            temp = s1.substr( initialPos, pos - initialPos);
-            
+            temp = s1.substr(initialPos, pos - initialPos);
             data[temp]++;
             
             initialPos = pos + 1;
             pos = s1.find(' ', initialPos);
         }
-
-        // Add the last one
-        temp = s1.substr( initialPos, min( pos, s1.size() ) - initialPos + 1 );
+        temp = s1.substr(initialPos, min(pos, s1.size()) - initialPos + 1);
         data[temp]++;
         
+        // String 2
         pos = s2.find(' ');
         initialPos = 0;
-
-        // Decompose statement
         
         while( pos != string::npos ) {
             temp = s2.substr(initialPos, pos - initialPos);
             data[temp]++;
             
             initialPos = pos + 1;
-
-            pos = s2.find(' ', initialPos );
+            pos = s2.find(' ', initialPos);
         }
-
-        // Add the last one
-        temp = s2.substr(initialPos, min(pos, s2.size()) - initialPos + 1 );
+        temp = s2.substr(initialPos, min(pos, s2.size()) - initialPos + 1);
         data[temp]++;
         
+        // Insert Data 
         for(pair<string,int> t: data){
             if(t.second == 1)
                 res.push_back(t.first);
