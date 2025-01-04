b133b1f7463ce99aa79768899c09fd3ca196ce86
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2025-01-04 15:47:48 +0530

    Time: 0 ms (100%), Space: 29 MB (35.62%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -10,33 +10,24 @@
  * };
  */
 class Solution {
-private:
-    bool isSimiler(TreeNode* root1, TreeNode* root2){
-        if(root1==nullptr && root2==nullptr) return true;
-        if(root1==nullptr || root2==nullptr) return false;
-        
-        if(root1->val != root2->val) return false;
-        
-        return isSimiler(root1->left, root2->left) && isSimiler(root1->right, root2->right);
-    }
 public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if((p == nullptr && q != nullptr) || (p != nullptr && q == nullptr)) return false;
+
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == nullptr){
-            return false;
-        }
-        bool ans = false;
+        if(root == nullptr && subRoot == nullptr) return true;
+        if(root == nullptr) return false;
+
         if(root->val == subRoot->val){
-            // check for similaritiy
-            ans = isSimiler(root, subRoot);
+            if(isSameTree(root, subRoot)){
+                return true;
+            }
         }
-        if(ans) return true;
-        
-        ans = isSubtree(root->left, subRoot);
-        if(ans) return true;
-        
-        ans = isSubtree(root->right, subRoot);
-        if(ans) return true;
-        
-        return false;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
