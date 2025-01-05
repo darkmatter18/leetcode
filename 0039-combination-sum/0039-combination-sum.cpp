0833f5db9e851712a878c5c30cbd08cf4fd441fe
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2025-01-05 21:17:33 +0530

    Time: 93 ms (7.91%), Space: 72.2 MB (6.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
-    
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> data;
-        
-        cS(candidates, target, 0, res, data);
-        
-        return res;
-    }
-    
-private:
-    void cS(vector<int>& candidates, int target, int idx, vector<vector<int>>& res, vector<int> curr){
-        if(target < 0 || idx >= candidates.size()) return;
-    
-        if(target==0){
-            res.push_back(curr);
+    void recursive(vector<int>& candidates, int idx, int target, vector<vector<int>>& res, vector<int> inter){
+        if(target == 0){
+            res.push_back(inter);
             return;
         }
-        
-        cS(candidates, target, idx+1, res, curr);
-        curr.push_back(candidates[idx]);
-        
-        cS(candidates, target-candidates[idx], idx, res, curr);
+        if(target < 0 || idx >= candidates.size()){
+            return;
+        }
+
+        inter.push_back(candidates[idx]);
+        recursive(candidates, idx, target-candidates[idx], res, inter);
+        inter.pop_back();
+        recursive(candidates, idx+1, target, res, inter);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> inter;
+
+        recursive(candidates, 0, target, res, inter);
+
+        return res;
     }
 };
